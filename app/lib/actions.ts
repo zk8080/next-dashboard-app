44e41058467bde81160e2f1f41f11ecf6939a19a
zk8080 <zhangkang8080@163.com>
2023-11-05 22:53:07 +0800

    feat: 第十二章使用Server Actions操作表单数据，以及revalidatePath和redirect方法使用

---
+++ app/lib/actions.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
+const UpdateInvoice = InvoiceSchema.omit({ date: true });
+
+export async function createInvoice(formData: FormData) {
+  const rawFormData = Object.fromEntries(formData.entries());
+  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  // 清除服务器路由缓存
+  revalidatePath("/dashboard/invoices");
+  // 重定向url
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const rawFormData = Object.fromEntries(formData.entries());
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    ...rawFormData,
+    id,
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  // 清除服务器路由缓存
+  revalidatePath("/dashboard/invoices");
+  // 重定向url
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+}
