ea5ea6c7fd9aebd72a47f25a7b58d8e4832da7df
zk8080 <zhangkang8080@163.com>
2023-11-07 22:10:38 +0800

    feat: Customer页面

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    query?: string;
+  };
+}) {
+  const { query } = searchParams;
+
+  return (
+    <Suspense fallback={<InvoicesTableSkeleton />}>
+      <CustomersTable query={query} />
+    </Suspense>
+  );
 }
